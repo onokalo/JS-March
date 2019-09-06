95482da2f6baa43e86f666b2e3623afd4d58f05f
Maryana Onokalo <hoptiyk@gmail.com>
2019-09-06 21:26:33 +0300

    update app.js

---
+++ 01.Git/js/app.js
@@ -0,0 +1,13 @@
+class Dog {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    ShowDogInfo = () => {
+        console.log("Name => ", this.name, "Age => ", this.age);
+    }
+}
+
+let Bobik = new Dog("Bobik", 3);
+Bobik.ShowDogInfo();
