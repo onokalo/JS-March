f0a90f2982a44e4413413b2af74ff63765cd7575
Maryana Onokalo <hoptiyk@gmail.com>
2019-09-27 17:07:15 +0300

    added new files

---
+++ NodeJS/02.HttpBase/server.js
@@ -0,0 +1,60 @@
+const http = require("http");
+
+const PORT = 81;
+
+const server = http.createServer((reg, res) => {
+    // console.log (reg.url);
+    const url = reg.url;
+    if(url==="/"){
+        // console.log("/ - works");
+        res.setHeader("Content-type:", "text/html");
+        // res.write("<h1>Hello World</h1>");
+        res.write("<html>");
+        res.write("<head>");
+        res.write("<title>Web Server</title>");
+        res.write("</head>");
+        res.write("<body>");
+        res.write("<main>");
+        res.write("<h1>Hello World</h1>");
+        res.write("</main>");
+        res.write("</body>");
+        res.write("</html>");
+        res.end();
+    }
+    else if (url==="/about"){
+        console.log("/about");
+        res.setHeader("Content-type:", "text/html");
+        res.write("<html>");
+        res.write("<head>");
+        res.write("<title>Web Server</title>");
+        res.write("</head>");
+        res.write("<body>");
+        res.write("<main>");
+        res.write("<h1>About us</h1>");
+        res.write("</main>");
+        res.write("</body>");
+        res.write("</html>");
+        res.end(person);
+    }
+    else{
+        console.log("Page not found!");
+        res.setHeader("Content-type:", "text/html");
+        res.write("<html>");
+        res.write("<head>");
+        res.write("<title>Web Server</title>");
+        res.write("</head>");
+        res.write("<body>");
+        res.write("<main>");
+        res.write("<h1>404 Page not found!</h1>");
+        res.write("</main>");
+        res.write("</body>");
+        res.write("</html>");
+        res.end();
+    }
+    
+})
+
+server.listen(PORT);
+// console.log(http);
+// console.log(http.STATUS_CODES);
+// console.log(http.METHODS);
